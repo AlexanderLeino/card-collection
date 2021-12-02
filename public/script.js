e64a50e6df96b35972a38203eaf3f7214517bd66
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-02 16:37:51 -0500

    connected front end to back end. Need to save data to database

--- public/script.js
+++ public/script.js
@@ -1,13 +1,28 @@
-const pokemonName = document.getElementById('pokemon-name')
-const pokemonType = document.getElementById('pokemon-type')
-const move1 = document.getElementById('move1')
-const move2 = document.getElementById('move2')
-const damage1 = document.getElementById('damage1')
-const damage2 = document.getElementById('damange2')
-const artist = document.getElementById('artist')
-const year = document.getElementById('year')
-const collection = document.getElementById('collection')
-const submitBtn = document.querySelector('button')
+let submitBtn = document.querySelector('button')
 
-console.log(pokemonName, pokemonType, move1, move2, damage1, damage2, artist, year, collection, submitBtn)
+submitBtn.addEventListener("click", async (e)=> {
+    e.preventDefault
+    const pokemonName = document.getElementById('pokemon-name').value
+    const pokemonType = document.getElementById('pokemon-type').value
+    const move1 = document.getElementById('move1').value
+    const move2 = document.getElementById('move2').value
+    const damage1 = document.getElementById('damage1').value
+    const damage2 = document.getElementById('damage2').value
+    const artist = document.getElementById('artist').value
+    const year = document.getElementById('year').value
+    const collection = document.getElementById('collection').value
+    await fetch('/api/pokemon', {
+        method: 'Post',
+        body: JSON.stringify({pokemonName, pokemonType, move1, move2, damage1, damage2, artist, year, collection}),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    
+    
+})
+
+
+
+
 
