d5fb47f03fc28bf28da0bf50144150f45649d4e4
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-02 15:16:59 -0500

    added ui

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const pokemonName = document.getElementById('pokemon-name')
+const pokemonType = document.getElementById('pokemon-type')
+const move1 = document.getElementById('move1')
+const move2 = document.getElementById('move2')
+const damage1 = document.getElementById('damage1')
+const damage2 = document.getElementById('damange2')
+const artist = document.getElementById('artist')
+const year = document.getElementById('year')
+const collection = document.getElementById('collection')
+const submitBtn = document.querySelector('button')
+
+console.log(pokemonName, pokemonType, move1, move2, damage1, damage2, artist, year, collection, submitBtn)
+
