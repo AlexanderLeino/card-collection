992327226be38ec1ba358115aba1988684dfafa4
Alexander Leino <Alexander.Leino@yahoo.com>
2021-12-05 14:43:52 -0500

    commit from laptop and clean up some routes

--- public/script.js
+++ public/script.js
@@ -14,10 +14,11 @@ submitBtn.addEventListener("click", (e)=> {
     const cardType = document.getElementById('card-type').value
     const year = document.getElementById('year').value
     const pickedCardSet = document.getElementById('pickedCardSet').value
+    const cardType = document.getElementById('card-type').value
     const cardRarity = document.getElementById('card-rarity')
     const quantity = document.getElementById('quantity').value
 
-    console.log(pokemonName, pokemonType, move1, move2, damage1, damage2, move1Description, move2Description, artist, cardType, pickedCardSet, cardRarity, quantity)
+    console.log(pokemonName, pokemonType, move1, move2, damage1, damage2, move1Description, move2Description, artist, cardType, pickedCardSet, cardType, cardRarity, quantity)
     fetch('/api/pokemon', {
         method: 'Post',
         body: JSON.stringify({pokemonName, pokemonType, move1, move2, damage1, damage2, move1Description, move2Description, artist, cardType, pickedCardSet, cardRarity, quantity}),
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,22 @@
 const router = require('express').Router()
-const db = require('../models/')
+const {Pokemon} = require('../models/')
 
 //matches api/pokemon
 router.post('/pokemon', ({body}, res) => {
     
     let {pokemonName, pokemonType, move1, move2, damage1, damage2, move1Description, move2Description, artist, pickedCardSet, cardRarity, quantity} = body
     
-    db.Pokemon.create({name: pokemonName, type: pokemonType, move1, move2, damage1, damage2, move1Description, move2Description, artist, pickedCardSet, cardRarity, quantity})
+    Pokemon.create({name: pokemonName, type: pokemonType, move1, move2, damage1, damage2, move1Description, move2Description, artist, pickedCardSet, cardRarity, quantity})
     res.send("We received the pokemon").status(200)
+})
+//matches api/pokemon
+router.get('/pokemon', async (req, res) => {
+    try {console.log('Get Route was hit')
+    let pokemonData = await Pokemon.find().populate('cardSet')
+    res.json(pokemonData).status(200)}
+    catch(e){
+        res.json(e).status(404)
+    }
 })
 
 module.exports = router
